66ec34383b3ef624cc2213602827a72f3eeabc5a
Nguyen063 <93272712+Nguyen063@users.noreply.github.com>
2022-12-28 01:41:20 +0700

    testcase

--- Search_test.js
+++ Search_test.js
@@ -4,3 +4,14 @@ Scenario('test something',  ({ I }) => {
     I.amOnPage('https://github.com');
     I.see('GitHub');
 });
+
+Scenario('test 2',  ({ I }) => {
+    I.amOnPage('https://www.demoblaze.com/');
+    I.click("//a[@id='login2']")
+    I.fillField("#loginusername", "admin");
+    I.fillField("#loginpassword", "admin");
+    I.click("//button[contains(text(),'Log in')]");
+    I.see('Sony');
+    I.wait(2)
+});
+
