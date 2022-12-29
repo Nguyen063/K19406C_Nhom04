ae5cb1939e07a6e63cbe22c12472614c42694f2e
Nguyen063 <93272712+Nguyen063@users.noreply.github.com>
2022-12-29 19:49:15 +0700

    unit

--- Login_test.js
+++ Login_test.js
@@ -1,17 +1,19 @@
-Feature('Search');
+Feature('Login');
 
 Scenario('test something',  ({ I }) => {
     I.amOnPage('https://github.com');
     I.see('GitHub');
+    I.wait(2);
 });
 
 Scenario('test 2',  ({ I }) => {
     I.amOnPage('https://www.demoblaze.com/');
     I.click("//a[@id='login2']")
+    I.wait(2);
     I.fillField("#loginusername", "admin");
     I.fillField("#loginpassword", "admin");
     I.click("//button[contains(text(),'Log in')]");
     I.see('Sony');
-    I.wait(2)
+    I.wait(2);
 });
 
